9f8a82fe9af282bafd5641dec2d01a5320548045
chensijie <chensijie@ledongli.me>
2015-07-11 19:22:25 +0800

    Initial Commit

---
+++ testAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testAlertView
+//
+//  Created by chensijie on 15/7/11.
+//  Copyright (c) 2015年 ledongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testAlertView
+//
+//  Created by chensijie on 15/7/11.
+//  Copyright (c) 2015年 ledongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
